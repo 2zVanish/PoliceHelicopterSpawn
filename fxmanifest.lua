1d784e994be646d8b8946054c104d2c8db73f90a
2zVanish <121704071+2zVanish@users.noreply.github.com>
2023-07-06 16:58:00 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'Police helicopter spawn marker that only police can acess' -- DO NOT CHANGE THIS (Give me some credit 😀)
+
+client_scripts {
+    'cl_helicoptermarker.lua'
+} 
